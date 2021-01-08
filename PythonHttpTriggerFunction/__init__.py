d12a681717c76df5b011890e3c6e9ddc901f2bbe
Jason Freeberg <jafreebe@microsoft.com>
2021-01-08 12:39:36 -0800

    Add initial files

---
+++ PythonHttpTriggerFunction/__init__.py
@@ -0,0 +1,31 @@
+import logging
+
+import azure.functions as func
+import numpy as np
+import pandas as pd
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    a = np.arange(15).reshape(3, 5)
+    logging.info(a)
+
+    s = pd.Series([1, 3, 5, np.nan, 6, 8])
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
